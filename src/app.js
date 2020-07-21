bf1f96175683e137ab2c260719de316e2431b77c
Joseph Garcia <prosif@gmail.com>
2020-07-20 21:23:26 -0700

    wip

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log('hello, world!');
+const board = document.getElementById('board');
+
+board.addEventListener('mousedown', () => {
+    const randomString = Math.random().toString(36);
+    window.location.replace('https://github.com/login/oauth/authorize?client_id=' + clientId + '&state=' + randomString);
+});
+
+const params = new URLSearchParams(location.search);
+
+const makePost = (url, data, cb) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.onreadystatechange = () => {
+        if (xhr.readystate > 3 && xhr.status == 200) {
+            cb && cb(xhr.responseText);
+	}
+    };
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    return xhr;
+};
+
+if (params.get('code')) {
+    console.log('github code');
+    console.log(params.get('code'));
+//    makePost(url, {'ayy': 'lmao'}, (data) => {
+//        console.log('hello');
+//	console.log(data);
+//    });
+}
